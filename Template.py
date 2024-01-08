251d7f1b9cd3f963cc77f43a3120d25f4d437b11
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-08 12:03:21 +0100

    Ajout template

---
+++ Template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: 
+
+@file: Template.py
+@created: /01/2024
+
+@project: pokemon 
+@licence: GPLv3
+"""
+
---
+++ menu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Cyril Génisson
+
+@file: menu.py
+@created: 08/01/2024
+
+@project: pokemon 
+@licence: GPLv3
+"""
+
